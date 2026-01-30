720469d44b213944d62a96eeaa4ae90e69185c34
Oliver Garrod <oliver.garrod@fabdata.io>
2026-01-30 11:43:56 +0000

    Updates to scrore_fineweb-edu-fortified (made into a typer script)

--- scripts/score_fineweb-edu-fortified/score_fineweb-edu-fortified.py
+++ scripts/score_fineweb-edu-fortified/score_fineweb-edu-fortified.py
@@ -1,17 +1,20 @@
 # %%
 from pathlib import Path
+import os
+from io import BytesIO
+from tempfile import NamedTemporaryFile
+import time
 
+import typer
 from datasets import Dataset, load_dataset, get_dataset_config_names
 import numpy as np
 from dotenv import load_dotenv
+from cloudpathlib import CloudPath, AzureBlobClient
 
 from qurating.constants import RESULTS_DIR
 from qurating.inference import ModelAnnotator, TokenizeAndChunk
 
-load_dotenv(override=True)
 
-
-# %%
 def init_dataset():
     ### configs are the different datasets (95, corresponding to CC dumps)
     configs = get_dataset_config_names("airtrain-ai/fineweb-edu-fortified")
@@ -92,49 +95,93 @@ def init_annotator():
 
     tokenizer = TokenizeAndChunk(str(model), "text", 512)
 
-    return annotator, tokenizer
+    return annotator, tokenizer, model
 
 
-# %%
-fw, subset_counts, fw_nshards = init_dataset()
+def main(start_partition: int, end_partition: int, n_partitions: int = 32):
+    load_dotenv(override=True)
+    AZURE_CLIENT_KWARGS = {
+        "account_url": "https://quratingscoressa.blob.core.windows.net",
+        "credential": os.getenv("AZURE_STORAGE_KEY"),
+    }
 
-# %%
-n_partitions = 32
-start_partition = 0
-end_partition = 1
+    fw, subset_counts, fw_nshards = init_dataset()
 
-subsets = get_partition_subsets(
-    start_partition, end_partition, n_partitions, subset_counts
-)
-
-# %%
-annotator, tokenizer = init_annotator()
-
-# %%
-subset = subsets[0]
-# for subset in subsets:
-
-keep_cols = ["id"]
-ds: Dataset = fw[subset]
-batch_cnt = 0
-max_batch_cnt = 1
-outer_batch_size = 10000
-res_list = []
-for batchi, batch_ds in enumerate(ds.batch(batch_size=outer_batch_size)):
-    if batch_cnt >= max_batch_cnt:
-        break
-    batch_cnt += 1
-    run_ds = Dataset.from_dict(batch_ds)
-    processed_ds = run_ds.map(tokenizer, batched=True, remove_columns=["text"])
-    results = processed_ds.map(
-        annotator,
-        batched=True,
-        with_indices=True,
-        batch_size=annotator.device_batch_size,
-        remove_columns=[
-            col for col in processed_ds.column_names if col not in keep_cols
-        ],
+    subsets = get_partition_subsets(
+        start_partition, end_partition, n_partitions, subset_counts
     )
-    out_cols = ["id", *[col for col in results.column_names if col.endswith("_average")]]
-    out_ds = results.select_columns(out_cols).add_column("dump", [subset]*len(results))
-    out_ds.to_parquet(f"{subset}_{batchi :04d}.parquet")
+
+    annotator, tokenizer, model_name = init_annotator()
+    model_string = [
+        substr for substr in Path(model_name).parts if substr.startswith("qurater_")
+    ]
+    assert len(model_string) == 1
+    model_string = model_string[0]
+
+    client = AzureBlobClient(**AZURE_CLIENT_KWARGS)
+
+    subset = subsets[0]
+    # for subset in subsets:
+
+    keep_cols = ["id"]
+    ds: Dataset = fw[subset]
+    batch_cnt = 0
+    max_batch_cnt = 3
+    outer_batch_size = 2000
+
+    for batchi, batch_ds in enumerate(ds.batch(batch_size=outer_batch_size)):
+        if batch_cnt >= max_batch_cnt:
+            break
+        batch_cnt += 1
+        run_ds = Dataset.from_dict(batch_ds)
+        processed_ds = run_ds.map(tokenizer, batched=True, remove_columns=["text"])
+        results = processed_ds.map(
+            annotator,
+            batched=True,
+            with_indices=True,
+            batch_size=annotator.device_batch_size,
+            remove_columns=[
+                col for col in processed_ds.column_names if col not in keep_cols
+            ],
+        )
+        ##########################
+        # create output dataset
+        print("Creating output dataset")
+        st = time.perf_counter()
+        out_cols = [
+            "id",
+            *[col for col in results.column_names if col.endswith("_average")],
+        ]
+        out_ds = results.select_columns(out_cols).add_column(
+            "dump", [subset] * len(results)
+        )
+        print(f"Elapsed time: {time.perf_counter() - st :.04f}")
+        # upload output dataset to blob
+        full_path = (
+            f"quratingscores/{model_string}/{subset}/{subset}_{batchi :04d}.parquet"
+        )
+        print(f"Uploading output dataset to: {full_path}")
+        st = time.perf_counter()
+        cloud_path = CloudPath(f"az://{full_path}", client=client)
+        with BytesIO() as bts:
+            out_ds.to_parquet(bts)
+            bts.seek(0)
+            cloud_path.write_bytes(bts.read())
+        print(f"Elapsed time: {time.perf_counter() - st :.04f}")
+        # check that blob is valid
+        # i.e. it can be loaded as dataset and values are identical to output dataset
+        print("Validating uploaded blob")
+        st = time.perf_counter()
+        with NamedTemporaryFile(mode="+wb") as f:
+            f.write(cloud_path.read_bytes())
+            f.seek(0)
+            print(f.name)
+            temp_ds = Dataset.from_parquet(f.name, keep_in_memory=True)
+        assert np.all(temp_ds.to_pandas() == out_ds.to_pandas()).item()
+        print(f"Elapsed time: {time.perf_counter() - st :.04f}")
+        time.sleep(0.1)
+        ###################
+
+
+if __name__ == "__main__":
+    typer.run(main)
