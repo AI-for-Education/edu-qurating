7d3a448817f0b6c3e6a93c4409d73293cd8a0eb4
ogarrod <oliver.garrod@fabdata.io>
2026-02-02 15:35:04 +0000

    Updated region priority order and added extra check for cuda after sleep

--- scripts/score_fineweb-edu-fortified/score_fineweb-edu-fortified.py
+++ scripts/score_fineweb-edu-fortified/score_fineweb-edu-fortified.py
@@ -58,23 +58,12 @@ def main(subsets: list[str]):
     }
 
     cuda_avail = torch.cuda.is_available()
-    print(cuda_avail)
-    print(torch.cuda.get_device_name(0))
-    print("__CUDNN VERSION:", torch.backends.cudnn.version())
-    print("__Number CUDA Devices:", torch.cuda.device_count())
-    print("__CUDA Device Name:", torch.cuda.get_device_name(0))
-    print(
-        "__CUDA Device Total Memory [GB]:",
-        torch.cuda.get_device_properties(0).total_memory / 1e9,
-    )
-    print("Memory Usage:")
-    print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024**3, 1), "GB")
-    print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024**3, 1), "GB")
-
     if not cuda_avail:
         time.sleep(30)
         cuda_avail = torch.cuda.is_available()
-        print(cuda_avail)
+        if not cuda_avail:
+            raise ValueError("CUDA not available")
+    else:
         print(torch.cuda.get_device_name(0))
         print("__CUDNN VERSION:", torch.backends.cudnn.version())
         print("__Number CUDA Devices:", torch.cuda.device_count())
@@ -86,8 +75,6 @@ def main(subsets: list[str]):
         print("Memory Usage:")
         print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024**3, 1), "GB")
         print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024**3, 1), "GB")
-        if not cuda_avail:
-            raise ValueError("CUDA not available")
 
     annotator, tokenizer, model_name = init_annotator()
     model_string = [
