257c43e3250a40b3b7184ef5b36c5450d9d99579
Oliver Garrod <oliver.garrod@fabdata.io>
2026-04-20 10:00:54 +0000

    Added download_checkpoints.py to scripts/rl

---
+++ scripts/rl/download_checkpoints.py
@@ -0,0 +1,38 @@
+# %%
+import subprocess
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(override=True)
+
+from qurating.constants import ROOT
+
+checkpoints_dir = (
+    "s3://qurating-checkpoints-183631302286-eu-west-2-an/checkpoints_grpo/"
+)
+
+HERE = Path(__file__).resolve().parent
+
+# %%
+run_names = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+    "test8",
+    "test9",
+    "test10",
+    "reasoning",
+]
+
+s5cmd_path = str(ROOT / ".venv/bin/s5cmd")
+
+for run_name in run_names:
+    cmd = [s5cmd_path, "--json"]
+    cmd += ["cp", "-n", "-s", "-u", "-sp"]
+    cmd += [f"{checkpoints_dir}{run_name}/*", str(HERE / "grpo" / f"{run_name}")]
+    subprocess.run(cmd)
