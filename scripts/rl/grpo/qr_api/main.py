84dbc9edf9386a09366aa3dd41662a5927a5bbfd
Oliver Garrod <oliver.garrod@fabdata.io>
2026-05-07 16:40:14 +0000

    added load_dotev to qr_api main.py

--- scripts/rl/grpo/qr_api/main.py
+++ scripts/rl/grpo/qr_api/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from datasets import Dataset
+from dotenv import load_dotenv
+
 from qr_scorer import QuratingScorer
+
+load_dotenv(override=True)
 
 qr_scorer = QuratingScorer()
 
@@ -22,15 +26,12 @@ def read_root():
 @app.post("/score")
 async def score(body: ScoreBody):
     if not body.texts:
-        scores_json = {f"{key}_average": [] for key in qr_scorer.labels[body.model_type]}
+        scores_json = {
+            f"{key}_average": [] for key in qr_scorer.labels[body.model_type]
+        }
     else:
         ds = Dataset.from_list([{"text": text} for text in body.texts])
         scores = qr_scorer._score_model(ds, model_type=body.model_type)
         scores_json = scores.to_dict()
 
-    return JSONResponse(
-        status_code=200,
-        content={
-            "scores": scores_json
-        }
-    )
+    return JSONResponse(status_code=200, content={"scores": scores_json})
